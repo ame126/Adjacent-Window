87783a82f37903ca1b57a45f27af068e24e0b59b
Arwa EL-Hawwat <ame126@scarletmail.rutgers.edu>
2021-11-08 15:37:01 -0500

    Adding user input functionality

--- js.js
+++ js.js
@@ -15,23 +15,3 @@ $(document).on('click', '.tabs > li > a', function(e) {
 		$(this).closest('ul').removeClass('active');
 	}
 });
-
-
-
-$(document).on('click', '.article > li > a', function(e) {
-	var $parent = $(this).parent();
-	if ($parent.is('.selected')) {
-		e.preventDefault();
-		// mobile support
-		$(this).closest('ul').addClass('active');
-	} else {
-		var target = $(this).attr('href');
-		$(target).siblings().removeClass('selected');
-		$(target).addClass('selected');
-		
-$parent.siblings().removeClass('selected');
-		$parent.addClass('selected');
-		// mobile support
-		$(this).closest('ul').removeClass('active');
-	}
-});
