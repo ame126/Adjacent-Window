eea88c59c517b9f3c6e619de7ae6ea1ddb7676d7
Srikar-Nomula <srikarreddynomula4@gmail.com>
2021-11-13 14:18:13 -0500

    Update code and included source js files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, session, request, redirect, url_for, jsonify, make_response
-from flask_bootstrap import Bootstrap
 import flask
 import flask_login
 from datetime import timedelta
@@ -9,7 +8,7 @@ import os
 
 '''Initialise the flask app '''
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+
 
 app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -19,6 +18,21 @@ UPLOAD_FOLDER = '\\storage'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
 logged_in = False
 
+login_cred = {
+    "testuser1": "test123",
+    "testuser2": "test456",
+    "testuser3": "test789"
+}
+
+
+def clear_session():
+    """
+    Clears the session variables
+    Args: None
+    :return:  None
+    """
+    session.pop('username', default=None)
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -27,17 +41,24 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')  # access the data inside
         password = request.form.get('password')
+        session['username'] = username
 
-        if username == 'testuser' and password == 'test123':
+        if username not in login_cred:
+            return render_template('login.html', message="Invalid username/password")
+
+        elif login_cred[username] != password:
+            return render_template('login.html', message="Invalid username/password")
+        else:
             flask.flash("Logged in Successfully")
             return render_template("index.html")
-        else:
-            flask.flash("Invalid username/password")
-    return render_template('login.html', message ="Invalid username/password")
+    else:
+        return render_template('login.html')
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -60,26 +81,39 @@ def upload_file():
     return jsonify(success=True)
 
 
-
-@app.route('/logout/<username>', methods=['GET','POST'])
+@app.route('/logout/<username>', methods=['GET', 'POST'])
 def logout(username):
     session.pop(username)
     print(session.keys())
 
 
-"""
-Handle error codes and relevant pages
-"""
 @app.errorhandler(404)
 def handle_bad_request(e):
+    """
+    Handle error codes and relevant pages
+    """
     return render_template("error.html")
+
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in session:
+            return render_template('login.html', message="Username already signedin")
+        else:
+            #new user
+            session['username'] = username
+            login_cred[username] = password
+            return render_template("index.html", message = "Successfully registered")
 
 
 @app.route('/', methods=['GET', 'POST'])
 # @flask_login.login_required
 def main():
-    global logged_in
-    if not logged_in:
+    if "username" not in session:
         return redirect(url_for("login"))
     else:
         return render_template("index.html")
--- static/js/js.js
+++ static/js/js.js
@@ -20,30 +20,6 @@ $(document).on('click', '.tabs > li > a', function(e) {
 });
 
 
-
-$(document).on('click', '.article > li > a', function(e) {
-	var $parent = $(this).parent();
-	console.log($parent);
-	console.log(e);
-	if ($parent.is('.selected')) {
-		e.preventDefault();
-		// mobile support
-		$(this).closest('ul').addClass('active');
-	} else {
-		var target = $(this).attr('href');
-		$(target).siblings().removeClass('selected');
-		$(target).addClass('selected');
-
-		$parent.siblings().removeClass('selected');
-		$parent.addClass('selected');
-		// mobile support
-		$(this).closest('ul').removeClass('active');
-	}
-});
-
-
-
-
 $('#upload_form').submit(function(e) {
 	e.preventDefault();
 	var form_data = new FormData($('#upload_form')[0]);
